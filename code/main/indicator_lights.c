5e121c8537d4d58636fb2e674a6fb1848c94a247
dowster <brady@dowster.net>
2019-12-15 14:47:38 -0600

    param not parm, this isn't cobol.

--- code/main/indicator_lights.c
+++ code/main/indicator_lights.c
@@ -2,6 +2,8 @@
 
 #include <driver/i2c.h>
 
+#include "mcp23017.h"
+
 #define CLUSTER_TYPE_E30
 #ifdef CLUSTER_TYPE_E30
 
@@ -45,7 +47,7 @@ void indicators_init() {
         .master.clk_speed = I2C_FREQUENCY
     };
 
-    i2c_parm_config(I2C_PORT, &conf);
+    i2c_param_config(I2C_PORT, &conf);
     i2c_driver_install(I2C_PORT, I2C_MODE_MASTER, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
 
     // Setup the i2c connection and configure the IO expander gpio
@@ -53,7 +55,8 @@ void indicators_init() {
     i2c_cmd_handle_t cmd = i2c_cmd_link_create;
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, EXPANDER_I2C_ADDRESS, I2C_ACK_ENABLE);
-    i2c_master_write_byte(cmd, 0x00, I2C_ACK_ENABLE);
+    i2c_master_write_byte(cmd, MCP23017_IOCONA, I2C_ACK_ENABLE);
+    i2c_master_write_byte(cmd, MCP23017_IOCON_BANK && 1, I2C_ACK_ENABLE);
     i2c_master_stop(cmd);
     i2c_master_cmd_begin(I2C_PORT, cmd, 50);
     i2c_cmd_link_delete(cmd);
